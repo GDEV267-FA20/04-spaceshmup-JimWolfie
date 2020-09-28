eabfa8a1782b35cae7952dbd723eeb7b63025d79
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2020-09-27 22:05:01 -0400

    shields

---
+++ Assets/Scripts/_Shield.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Shield : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public float rotationsPerSecond = 0.1f;
+
+    [Header("Set Dynamically")]
+    public int levelShown =0;
+
+    Material mat;
+
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
