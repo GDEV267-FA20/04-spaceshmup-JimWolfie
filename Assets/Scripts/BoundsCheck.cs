e675133df2bc2549d38e3e14703028f0f767a641
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2020-09-28 19:04:15 -0400

    code

--- Assets/Scripts/BoundsCheck.cs
+++ Assets/Scripts/BoundsCheck.cs
@@ -6,10 +6,15 @@ public class BoundsCheck : MonoBehaviour
 {
     [Header("Set in Inspector")]
     public float radius =1f;
+    public bool keepOnScreen = true;
 
     [Header("Set Dynamically")]
+    public bool isOnScreen = true;
     public float camWidth;
     public float camHeight;
+
+    [HideInInspector]
+    public bool offRight, offLeft, offUp, offDown;
 
     private void Awake()
     {
@@ -19,24 +24,39 @@ public class BoundsCheck : MonoBehaviour
     private void LateUpdate()
     {
         Vector3 pos = transform.position;
+        isOnScreen = true;
+        offRight = offLeft = offDown = offUp = false;
+
         if(pos.x > camWidth - radius)
         {
             pos.x= -camWidth+radius;
+            offRight = true;
         }
         if(pos.x < -camWidth+radius)
         {
-            pos.x= camWidth - radius;
+            pos.x= camWidth - radius;          
+            offLeft = true;
         }
         if(pos.y>camHeight-radius)
         {
             pos.y = -camHeight+radius;
+            offUp = true;
         }
         if(pos.y<-camHeight+radius)
         {
             pos.y= camHeight-radius;
+            offDown = true;
         }
-        transform.position=pos;
+        isOnScreen = !(offRight||offLeft||offDown||offUp);
+        if(keepOnScreen && !isOnScreen)
+        {
+            transform.position=pos;
+            isOnScreen=true;
+            offRight = offLeft = offDown = offUp = false;
+        }
+        
     }
+    //swap the +- values on camwidth/height and the respecitve radi to keep stuff in bounds
     private void OnDrawGizmos()
     {
         if(!Application.isPlaying)return;
