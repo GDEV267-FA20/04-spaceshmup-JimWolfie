5913c9d08e441ba7c913617ffd28cec15584463b
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2020-09-28 20:21:53 -0400

    ok

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private BoundsCheck bndCheck;
 
-    // Update is called once per frame
+    private void Awake()
+    {
+        bndCheck = GetComponent<BoundsCheck>();
+    }
+  
+    
     void Update()
     {
-        
+        if(bndCheck.offUp)
+        {
+            Destroy(gameObject);
+        }
     }
 }
