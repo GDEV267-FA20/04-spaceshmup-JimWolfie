7ec5777815bf9bdf2449264cbe1dd31dc3cdee18
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2020-09-29 20:49:03 -0400

    omg

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,10 +5,27 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     private BoundsCheck bndCheck;
+    private Renderer rend;
+
+    [Header("set dynamically")]
+    public Rigidbody rb;
+    [SerializeField]private WeaponType _type;
+
+    public WeaponType type
+    {
+        get {
+            return(_type);
+        }
+        set {
+            SetType(value);
+        }
+    }
 
     private void Awake()
     {
         bndCheck = GetComponent<BoundsCheck>();
+        rend = GetComponent<Renderer>();
+        rb = GetComponent<Rigidbody>();
     }
   
     
@@ -18,5 +35,11 @@ public class Projectile : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+    public void SetType(WeaponType eType)
+    {
+        _type = eType;
+        WeaponDefinition def = Main.GetWeaponDefinition(_type);
+        rend.material.color = def.projectileColor;
     }
 }
